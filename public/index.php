7f2410243305845bca32e32010b57bac93ec14a6
RicardoERR <reriverosr@gmail.com>
2020-12-07 21:28:43 -0300

    Eliminar archivos innecesarios

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 require_once __DIR__
 . '/../vendor/autoload.php';
 
-// $base_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://". $_SERVER['HTTP_HOST'].(isset($_SERVER['SERVER_PORT'])? ':'.$_SERVER['SERVER_PORT'] : '').'/';
 $base_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://". $_SERVER['HTTP_HOST'].'/';
 
 $Operaciones = array("envías","recibes");
